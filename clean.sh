da17eab8ddf500f7690c181ba6c9d9a08844fb3f
Cloud Genius <user@cloudgeni.us>
2018-02-12 20:38:00 +0000

    edits

--- clean.sh
+++ clean.sh
@@ -5,9 +5,13 @@ bundle exec knife client delete docloudgenius -y
 bundle exec knife node delete ec2cloudgenius -y
 bundle exec knife client delete ec2cloudgenius -y
 
+bundle exec knife cookbook bulk delete '.*' -y
+
 bundle exec knife role from file roles/*.rb
+bundle exec knife environment from file environments/*.rb
+
+bundle exec knife cookbook upload -o ./cookbooks/ --all
 bundle exec knife cookbook upload -o ./site-cookbooks/ --all
 
-
-bundle exec knife digital_ocean droplet destroy --all
 bundle exec knife ec2 server delete -N ec2cloudgenius -y
+bundle exec knife digital_ocean droplet destroy --all
