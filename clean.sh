71776a9c4b071e295cdb055100535166ce80f469
Cloud Genius <user@cloudgeni.us>
2018-02-13 01:02:54 +0000

    another run

--- clean.sh
+++ clean.sh
@@ -10,7 +10,7 @@ bundle exec knife cookbook bulk delete '.*' -y
 bundle exec knife role from file roles/*.rb
 bundle exec knife environment from file environments/*.rb
 
-bundle exec knife cookbook upload -o ./cookbooks/ --all
+bundle exec knife cookbook upload -o ~/.berkshelf/cookbooks/ --all
 bundle exec knife cookbook upload -o ./site-cookbooks/ --all
 
 bundle exec knife ec2 server delete -N ec2cloudgenius -y
